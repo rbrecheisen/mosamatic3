f94650eb88cc02488f2bb05632556951b683c431
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2026-06-11 12:35:30 +0200

    Fixed bug run-server.sh

--- mosamatic3/serveronly/run-createarchive.sh
+++ mosamatic3/serveronly/run-createarchive.sh
@@ -4,22 +4,22 @@ root="$HOME/Documents/Development/GitHub/mosamatic3/mosamatic3/serveronly"
 zipFilePath="$root/archive.zip"
 
 itemsToInclude=(
-  "config",
-  "core",
-  "docker",
-  "nginx",
-  "static",
-  "templates",
-  ".env.example",
-  "docker-compose.yml",
-  "docker-compose-dev.yml",
-  "docker-deploy2hub.ps1",
-  "docker-pullfromhub-runall.ps1",
-  "docker-runall.ps1",
-  "docker-runbackendservices.ps1",
-  "Dockerfile",
-  "manage.py",
-  "pyproject.toml",
+  "config"
+  "core"
+  "docker"
+  "nginx"
+  "static"
+  "templates"
+  ".env.example"
+  "docker-compose.yml"
+  "docker-compose-dev.yml"
+  "docker-deploy2hub.ps1"
+  "docker-pullfromhub-runall.ps1"
+  "docker-runall.ps1"
+  "docker-runbackendservices.ps1"
+  "Dockerfile"
+  "manage.py"
+  "pyproject.toml"
   "uv.lock"
 )
 
