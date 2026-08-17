fb5ad00c96cc8a7a1d7676ee9a7bdd362c0b0b48
Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2026-08-17 13:44:44 +0200

    Saving work

--- mosamatic3/server/run-dockerall.ps1
+++ mosamatic3/server/run-dockerall.ps1
@@ -1,3 +1,4 @@
 docker compose -f docker-compose-dev.yml down
 docker compose -f docker-compose-dev.yml up -d --build
-docker compose -f docker-compose-dev.yml logs -f
+
+powershell -NoProfile -ExecutionPolicy Bypass -Command "docker compose -f docker-compose-dev.yml logs -f --no-color 2>&1 | Tee-Object -FilePath '.\data\logs\docker-compose.log' -Append"
